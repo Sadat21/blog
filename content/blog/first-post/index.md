f0e7f91ad058a067cd769ebc11ae9077bfe6d22f
Sadat Islam <sadat21@hotmail.ca>
2024-09-23 19:00:44 -0600

    Clean up template files

---
+++ content/blog/first-post/index.md
@@ -0,0 +1,9 @@
+---
+title: First Post
+date: "2024-09-23T22:12:03.284Z"
+description: "Hello World!"
+---
+
+This is my first post on my new blog! How exciting!
+
+I'm sure I'll write a lot more interesting things in the future.
--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Sadat's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Sadat Islam`,
+      summary: `a software engineer who grew up in Calgary, Canada.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `A blog of things I've learned throughout my career.`,
+    siteUrl: `https://sadat21.github.io/blog/`,
     social: {
-      twitter: `kylemathews`,
+      linkedin: `sadat-islam`,
     },
   },
   pathPrefix: "/blog",
@@ -103,7 +103,7 @@ module.exports = {
               }
             }`,
             output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
+            title: "Sadat's Blog RSS Feed",
           },
         ],
       },
@@ -111,8 +111,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Sadat Islam's Blog`,
+        short_name: `SI Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
